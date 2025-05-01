e2b5fdc19f6ef8a76d29cf45f3fa9ee06ec39e0b
Mega <110767300+Moeketsi1@users.noreply.github.com>
2025-05-01 23:22:13 +0200

    Update firebase-config.js

--- Election_Platform_System/Frontend/js/firebase-config.js
+++ Election_Platform_System/Frontend/js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArMcYsmynZj9ZTU-bVrS1Cd0sY-uTJ_q4",
-  authDomain: "election-platform-2bd6e.firebaseapp.com",
-  projectId: "election-platform-2bd6e",
-  storageBucket: "election-platform-2bd6e.firebasestorage.app",
-  messagingSenderId: "217515421593",
-  appId: "1:217515421593:web:ccc375f261a9f1b3846d08"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
